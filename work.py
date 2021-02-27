bb5966493e767c9476877e08794690208ea41e4b
user12 <kazak.stepan@list.ru>
2021-02-27 21:16:50 +0300

    home work 1

--- work.py
+++ work.py
@@ -1,2 +1,9 @@
-print('hello')
-input('введите имя')
+name = 'Bill'
+print('hello', name)
+age = 33
+print('your old',age)
+list = int(input('Введите число от 1 до 10: '))
+test = str(input('Введите строку с названием числа: '))
+print('Ваше число:', list)
+print('Его название:', test)
+
