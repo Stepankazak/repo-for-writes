56e4cf3eb323eb498652aec9df052e50c02ec051
user12 <kazak.stepan@list.ru>
2021-02-28 19:08:39 +0300

    home work 1

--- one,Gybareva.py
+++ one,Gybareva.py
@@ -1,9 +1,6 @@
-name = 'Bill'
-print('hello', name)
-age = 33
-print('your old',age)
-list = int(input('Введите число от 1 до 10: '))
-test = str(input('Введите строку с названием числа: '))
-print('Ваше число:', list)
-print('Его название:', test)
 
+time = int(input('Введите время в секундах: '))
+hour = time // 3600
+minute =  time % 3600 // 60
+second = time % 60
+print(f'{hour}: {minute}: {second}')
---
+++ work3.py
@@ -0,0 +1,4 @@
+n  = input('Введите число n: ')
+nn = int(n + n)
+nnn = int(n + n + n)
+print(int(n) + nn + nnn)
