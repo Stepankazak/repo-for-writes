0723f9f873c9632823f4e09ad431bd914a44db46
user12 <kazak.stepan@list.ru>
2021-03-01 12:38:47 +0300

    home work 1

---
+++ one,Gybareva.py
@@ -0,0 +1,7 @@
+name = 'Bob'
+age = 50
+print(name, age)
+test = int(input('Введите число: '))
+text = input('Введите его развание: ')
+print('Вы ввели число',test)
+print('Его название', text)
---
+++ work.py
@@ -0,0 +1,7 @@
+time = int(input('Введите время в секундах: '))
+hour = time // 3600
+minute = time % 3600 // 60
+second = time % 60
+print(f'{hour}:{minute}:{second}')
+
+
