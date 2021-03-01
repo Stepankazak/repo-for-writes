7c54be5d444ebee34eb605ff4c1474add21531a4
user12 <kazak.stepan@list.ru>
2021-03-01 09:09:28 +0300

    home work 1

---
+++ work4.py
@@ -0,0 +1,10 @@
+number = int(input('Введите целое положительное число: '))
+big = number % 10
+number = number // 10
+while number > 10:
+    if number % 10 > big:
+        big = number % 10
+    number = number // 10
+
+print(big)
+print('Конец программы')
