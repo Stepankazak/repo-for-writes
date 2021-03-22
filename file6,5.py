a66871bf341daf35a479f9e02ed339b85e07d9d0
user12 <kazak.stepan@list.ru>
2021-03-22 20:56:58 +0300

    hw6

---
+++ file6,5.py
@@ -0,0 +1,22 @@
+class Stationary:
+
+    def __init__(self, title="Something that can draw"):
+        self.title = title
+
+    def draw(self):
+        print(f'Just start drawing! {self.title}')
+
+class Pen(Stationary):
+    def draw(self):
+        print(f'Just start drawing with pen! {self.title}')
+
+class Pencil(Stationary):
+    def draw(self):
+        print(f'Just start drawing with Pencil! {self.title}')
+
+class Marker(Stationary):
+    def draw(self):
+        print(f'Just start drawing with Marker! {self.title}')
+
+stat = Stationary()
+stat.draw()
